bd4fe92f5448b7b973e860da59f847fc83d1ee98
Michal Ozogán <michal@ozogan.eu>
2018-10-04 14:44:18 +0200

    InProgress: typescript

--- src/index.js
+++ src/index.js
@@ -1,143 +1,101 @@
-const babel = require('rollup-plugin-babel');
-const resolve = require('rollup-plugin-node-resolve');
-const replace = require('rollup-plugin-re');
-const replaceDist = require('rollup-plugin-replace');
-const commonjs = require('rollup-plugin-commonjs');
 const fs = require('fs');
 const path = require('path');
-const packageJson = require('./../package.json');
-const { getFiles, onGenerate, getBaseBabelConfig } = require('./helpers');
+const babel = require('rollup-plugin-babel');
+const resolve = require('rollup-plugin-node-resolve');
+const serve = require('rollup-plugin-serve');
+const getBabelConfig = require('@socifi/babel-config');
 
-/**
- * Get basic config for rollup.
- *
- * @returns {Object} config
- */
-function getBasicConfig() {
+function getFiles(baseDir) {
+    // get all items from base directory
+    const items = fs.readdirSync(baseDir);
+
+    // go through
+    return Object.keys(items).map((key) => {
+        const item = items[key];
+
+        // check if item is directory
+        if (fs.lstatSync(path.join(baseDir, item)).isDirectory()) {
+            // if so, ready all files in it
+            return getFiles(path.join(baseDir, item));
+        }
+
+        // otherwise just return full path
+        return path.join(baseDir, item);
+    }).reduce((a, b) => a.concat(b), []);
+}
+
+function getFileConfig(file, settings = {}) {
+    const destinationFile = file.replace('src', 'dist');
+    const fileName = path.parse(file).base;
+
     return {
         treeshake: {
             propertyReadSideEffects: false,
             pureExternalModules: true,
         },
-    };
-}
-
-/**
- * Get config for develop bundle.
- *
- * @param {string} dir - Path of project
- * @param {?Object} options - Optional settings
- * @returns {Object} Rollup config
- */
-function getDevConfig(dir, options = {}) {
-    const basicConfig = getBasicConfig();
-    const babelConfig = getBaseBabelConfig(false, {
-        browsers: packageJson.browserslist,
-    });
-
-    babelConfig.plugins = [
-        [
-            'flow-runtime',
+        input: file,
+        output: [
             {
-                assert: true,
-                annotate: true,
+                file: destinationFile.replace(/\.(tsx|ts)$/, '.js'),
+                format: 'cjs',
+                freeze: false,
             },
-        ],
-        ...babelConfig.plugins.filter(plugin => plugin !== 'external-helpers'),
-    ];
-
-    return {
-        ...basicConfig,
-        input: path.resolve(dir, 'dev', 'index.js'),
-        output: [{
-            file: path.resolve(dir, 'dev', 'index.build.js'),
-            format: 'umd',
-            name: 'dev',
-        }],
-        plugins: [
-            replaceDist({
-                'process.env.NODE_ENV': JSON.stringify('dev'),
-            }),
-            babel(babelConfig),
-            resolve({
-                jsnext: true,
-            }),
-            commonjs({
-                namedExports: {
-                    'node_modules/react/react.js': ['Children', 'Component', 'PropTypes', 'createElement', 'isValidElement'],
-                    'node_modules/react-dom/index.js': ['render', 'createPortal'],
-                    'node_modules/react-intl/lib/index.es.js': ['IntlShape'],
-                    'node_modules/airbnb-prop-types/index.js': ['forbidExtraProps', 'nonNegativeInteger', 'or', 'childrenOfType'],
-                },
-            }),
-        ],
-        ...options,
-    };
-}
-
-/**
- * Default configuration for rollup.
- *
- * @param {Object} packageConfig - Parsed package.json
- * @param {string} baseDir - Base dir of plugin
- * @param {Object} options - Optional parameters
- * @returns {Array<Object>} Rollup settings
- */
-function getLibraryConfig(packageConfig = packageJson, baseDir, options = {}) {
-    const basicConfig = getBasicConfig(packageConfig);
-    const libraryToReplace = ['ui-constants', 'ui-models', 'ui-admin-api-service'];
-
-    return getFiles(baseDir).map((file) => {
-        const destinationFile = file.replace('src', 'dist');
-        const fileName = path.parse(file).base;
-        const isMainIndex = path.resolve(file) === path.resolve(baseDir, fileName) && fileName === 'index.js';
-        return {
-            ...basicConfig,
-            input: file,
-            output: [
+            ...(fileName.indexOf('index') >= 0 ? [
                 {
-                    file: destinationFile,
-                    format: 'cjs',
+                    file: `${destinationFile.replace(/\.(tsx|ts)$/, '.es.js')}`,
+                    format: 'es',
                     freeze: false,
                 },
-                ...(isMainIndex ? [
-                    {
-                        file: `${destinationFile.replace('js', 'es.js')}`,
-                        format: 'es',
-                        freeze: false,
-                    },
-                ] : []),
-            ],
-            plugins: [
-                replace({
-                    patterns: libraryToReplace.map((library) => {
-                        return {
-                            test: `${library}/src`,
-                            replace: `${library}/dist`,
-                        };
-                    }),
-                }),
-                resolve({
-                    modulesOnly: true,
-                    customResolveOptions: {
-                        moduleDirectory: 'src',
-                    },
-                    extensions: ['.js', '.jsx'],
-                }),
-                babel(getBaseBabelConfig(false, {
-                    browsers: packageConfig.browserslist,
-                })),
-                onGenerate(() => {
-                    fs.copyFile(file, `${destinationFile}.flow`, () => {});
-                }),
-            ],
-            ...options,
-        };
-    });
+            ] : []),
+        ],
+        plugins: [
+            babel(getBabelConfig(undefined, false)),
+            resolve({
+                modulesOnly: true,
+                customResolveOptions: {
+                    moduleDirectory: 'src',
+                },
+                extensions: ['.js', '.jsx', '.ts', '.tsx'],
+            }),
+        ],
+        ...settings,
+    };
+}
+
+function getDirectoryConfig(dir, settings) {
+    return getFiles(dir).map(file => getFileConfig(file, settings));
+}
+
+function getDevelopConfig(file) {
+    const base = getFileConfig(file);
+    const destinationFile = file.replace(/dev$/, 'build').replace(/\.(tsx|ts)$/, '.js');
+
+    fs.copyFileSync(
+        path.resolve(__dirname, '..', 'assets', 'index.html'),
+        path.resolve(path.parse(destinationFile).dir, 'index.html')
+    );
+
+    return {
+        ...base,
+        plugins: [
+            ...base.plugins,
+            serve({
+                contentBase: 'build',
+                port: 8080,
+            }),
+        ],
+        output: [
+            {
+                file: destinationFile,
+                format: 'cjs',
+                freeze: false,
+            },
+        ],
+    };
 }
 
 module.exports = {
-    default: getLibraryConfig,
-    getLibraryConfig,
-    getDevConfig,
+    getFileConfig,
+    getDirectoryConfig,
+    getDevelopConfig,
 };
